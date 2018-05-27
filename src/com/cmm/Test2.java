e2a6b461efae9ad53f2d648fae19b2aaf2536979
alicecmm <516510275@qq.com>
2018-05-27 13:19:19 +0800

    dsfdsfsd

--- src/com/cmm/Test2.java
+++ src/com/cmm/Test2.java
@@ -3,7 +3,7 @@ package com.cmm;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("cmmm is a big pig!!!");
+		System.out.println("cmmm is a good coding!!!");
 
 	}
 
